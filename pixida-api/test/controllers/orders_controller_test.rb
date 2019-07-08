79c02b29931fd7fda5ee4a7e447d73e739f24131
github username <github email address>
2019-07-08 15:24:30 -0400

    restructured user and order model

---
+++ pixida-api/test/controllers/orders_controller_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class OrdersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @order = orders(:one)
+  end
+
+  test "should get index" do
+    get orders_url, as: :json
+    assert_response :success
+  end
+
+  test "should create order" do
+    assert_difference('Order.count') do
+      post orders_url, params: { order: { comeback: @order.comeback, item_type: @order.item_type, order_number: @order.order_number, service_type: @order.service_type } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show order" do
+    get order_url(@order), as: :json
+    assert_response :success
+  end
+
+  test "should update order" do
+    patch order_url(@order), params: { order: { comeback: @order.comeback, item_type: @order.item_type, order_number: @order.order_number, service_type: @order.service_type } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy order" do
+    assert_difference('Order.count', -1) do
+      delete order_url(@order), as: :json
+    end
+
+    assert_response 204
+  end
+end
